122d44e4d513cb9a88ca56b95f3e75e7a8e32ab7
pratham-sggs <2022bit045@sggs.ac.in>
2024-04-17 22:51:59 +0530

    Edited ISE Practice

--- ISE-Practice/Innerclass.java
+++ ISE-Practice/Innerclass.java
@@ -3,31 +3,32 @@ import java.io.*;
 
 class Ex
 {
-	private String name; 
+	private String name;
+	public int number;
 	class error
 	{
-		private String name;
+		private String nam1e;
+		public int number;
 		public void Hello()
 		{
 			System.out.println("In error Hello");
 			error e = new error();
-			e.name = "Pratham";
-			System.out.println(e.name);
+			name = "Pratham";
+			System.out.println(name);
+			Ex.this.name = "Prathamhhh";
+			System.out.println(Ex.this.name);
+			this.nam1e = "Pratham hhh Ex";
+			System.out.println(nam1e);
+			this.number = 10;
+			System.out.println(number);
 		}
 	}
-	public void Helloc()
-		{
-			System.out.println("In Ex Hello");
-			Ex e = new Ex();
-			e.name = "Pratham Ex";
-			System.out.println(e.name);
-		}
 	public static void main(String ...args)
 	{
 		
 		Ex k = new Ex();
-		error c = k.new error();
+		Ex.error c = k.new error();
 		c.Hello();
-		k.Helloc();
+		
 	}
 }
